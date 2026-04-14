93665e1c85f457f6317aa88aaac01a256ac22685
arnaldo felipe <feelipesilvaa11@gmail.com>
2026-04-13 22:50:35 -0300

    criação do componente de login e comunicação com a API

---
+++ src/app/core/services/auth.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginRequest, LoginResponse } from '../models/auth.model';
+import { Observable, tap } from 'rxjs';
+import { Token } from '@angular/compiler';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+
+  private api = 'http://localhost:8080/auth/login';
+
+  constructor(private http: HttpClient){}
+
+  login(credenciais: LoginRequest): Observable<LoginResponse>{
+    return this.http.post<LoginResponse>(this.api, credenciais).pipe(
+      tap((response)=> {
+        localStorage.setItem('token', response.token)
+      })
+    );
+  }
+
+  logout(): void{
+    localStorage.removeItem('token');
+  }
+
+  getToken(): string | null{
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated(): boolean{
+    return !!this.getToken;
+  }
+}
---
+++ src/app/features/auth/login/login.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { LoginRequest } from '../../../core/models/auth.model';
+import { AuthService } from '../../../core/services/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss'],
+})
+export class Login {
+
+  private fb = inject(FormBuilder)
+
+  constructor(private auth:AuthService,
+              private router:Router
+  ){}
+
+  form = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    senha: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+  entrar(): void{
+    if (this.form.invalid){
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const credenciais: LoginRequest =  this.form.getRawValue();
+
+    this.auth.login(credenciais).subscribe({
+      next: () => {
+        this.router.navigate(['/home']);
+      },
+      error: () => {
+        console.log('Erro ao realizar login');
+      }
+    });
+  }
+}
